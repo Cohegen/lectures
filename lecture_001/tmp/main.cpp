b03168719fea5202069fe81a9368c0a47058c092
Antonius <161943308+Cohegen@users.noreply.github.com>
2025-08-07 20:19:44 +0300

    Update main.cpp

--- lecture_001/tmp/main.cpp
+++ lecture_001/tmp/main.cpp
@@ -7,3 +7,7 @@ std::string hello_world() {
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 m.def("hello_world", torch::wrap_pybind_function(hello_world), "hello_world");
 }
+
+at::Tensor square_kernel(at::Tensor input) {
+    return input * input;
+}
